156c26c84ad8f15e1f82a8115631d9a7decc8089
OfficialPapu <papuyadavofficial386@gmail.com>
2024-05-17 17:36:43 +0545

    Suppliers page error fixed

--- Suppliers.h
+++ Suppliers.h
@@ -1,31 +1,32 @@
 void SuppliersData(){
-    int  FileExistData, SuppliersMobile,TotalCost, LoopCount,i;
-	char  SuppliersName[300], *Data;
+    int  FileExistData, LoopCount,i;
+	long long SuppliersMobile,TotalCost;
+	char  SuppliersName[300], line[100];
 	FILE *SuppliersFilePointer;
 	system("cls");
 	printf("Enter the suppliers name: ");
 	scanf("%s",SuppliersName);
 	system("cls");
 	printf("Enter Suppliers Mobile: ");
-	scanf("%d",&SuppliersMobile);
+	scanf("%lld",&SuppliersMobile);
 	system("cls");
 	printf("Enter Total Purchase Cost: ");
-	scanf("%d",&TotalCost);
+	scanf("%lld",&TotalCost);
 	FileExistData=fopen("Suppliers.csv","r");
 	if(FileExistData != NULL){
 		int tempID, maxID=1;
-	while (fscanf(FileExistData,"\n%d",&tempID) != EOF ){
-		printf("The val of tempid %d",tempID);
-		    if (tempID > maxID) {
+	while (fgets(line, sizeof(line),FileExistData)){
+			sscanf(line,"%d",&tempID);
+		    if (tempID >= maxID) {
             maxID = tempID;
         }
 	}
 	SuppliersFilePointer=fopen("Suppliers.csv","a");
-	fprintf(SuppliersFilePointer,"\n%d, %s, %d, %d",maxID,SuppliersName,SuppliersMobile,TotalCost);
+	fprintf(SuppliersFilePointer,"\n%d, %s, %lld, %lld",tempID+1,SuppliersName,SuppliersMobile,TotalCost);
 	}else{
 	SuppliersFilePointer=fopen("Suppliers.csv","w");
 	fprintf(SuppliersFilePointer,"ID, Suppliers Name, Suppliers Mobile, Total Cost");	
-	fprintf(SuppliersFilePointer,"\n1, %s, %d, %d",SuppliersName,SuppliersMobile,TotalCost);
+	fprintf(SuppliersFilePointer,"\n1, %s, %lld, %lld",SuppliersName,SuppliersMobile,TotalCost);
 	}
 	fclose(FileExistData);
     }
